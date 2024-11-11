a8acb8de91faffcb6a7f7546997898801e124bc9
Henry <henry.arcangello@proton.me>
2024-11-11 16:38:43 +0000

    update commands/fallout/index.mjs

--- commands/fallout/index.mjs
+++ commands/fallout/index.mjs
@@ -89,7 +89,7 @@ export async function outro() {
 function generateDynamicLine(char = "━") {
     // Get the width of the screen in pixels
     const screenWidth = window.innerWidth
-    let adds = screenWidth/4
+    let adds = screenWidth/8
     // Approximate width of the character in pixels (adjust based on your font size)
     const charWidth = 5; // Adjust if your font is different
     const charCount = Math.floor(screenWidth / charWidth)/3 + adds ;
@@ -112,7 +112,7 @@ function generateDynamicLine(char = "━") {
       let paddingSpaces = Math.max(0, Math.floor((viewportWidth - textWidth) / 2));
 
       if(window.innerWidth < 420) {
-        paddingSpaces = Math.max(0, Math.floor((viewportWidth - textWidth) / 4));
+        paddingSpaces = Math.max(0, Math.floor((viewportWidth - textWidth) / 6));
       }
       
       // Return the text with spaces before it
