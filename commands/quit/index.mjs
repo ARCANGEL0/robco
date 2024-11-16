897727a45a3956ed76854ad916d815277d594d1a
Henry <henry.arcangello@proton.me>
2024-11-16 15:14:37 +0000

    asdsad

--- commands/quit/index.mjs
+++ commands/quit/index.mjs
@@ -1,6 +1,21 @@
 import { off } from "../../util/power.js";
 
-const output = "Goodbye.";
+const output = [
+    "ROBCO TERMLINK",
+    "",
+    "STATUS: SHUTTING DOWN",
+    "",
+    "WARNING: SYSTEM INITIATED SHUTDOWN SEQUENCE.",
+    "",
+    "All operations will cease in T-minus 60 seconds.",
+    "Please ensure that all data has been saved and all activities are concluded.",
+    "",
+    "NOTE: Unauthorized access during shutdown may result in data corruption.",
+    "",
+    "Thank you for using the ROBCO TERMLINK. Your connection has been appreciated.",
+    "",
+    "SYSTEM POWERING DOWN..."
+];
 
 export default () => {
 	return off();
