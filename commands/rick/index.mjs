fc5c8cc6b34ba2ff031a882dc19a41a958a9c5c2
Henry <henry.arcangello@proton.me>
2024-11-24 19:20:15 +0000

    update commands/rick/index.mjs

--- commands/rick/index.mjs
+++ commands/rick/index.mjs
@@ -1,6 +1,52 @@
-const output = [
-	"Available commands:",
-	"help, fallout, brogue, dino, hackerman, matrix, screensaver, text-editor, cowsay, fire, clear, logout, reboot, quit"
-];
+// friend.mjs
 
-export { output };
+import { getScreen, showTemplateScreen, addTemplate, clear } from "../../util/screens.js";
+import { type, waitForKey,input, cleanInput,isPrintable,moveCaretToEnd  } from "../../util/io.js";
+
+import pause from "../../util/pause.js";
+import { typeSound } from "../../sound/index.js"
+import say from "../../util/speak.js";
+
+
+    
+
+
+async function rick() {
+  
+  
+
+	clear();
+	say("you got rick rolled!", 0.3, 0.8);
+	
+	return new Promise(async resolve =>
+	{
+
+
+let rickrolled = new Audio('commands/rick/rick.mp3');
+	rickrolled.play();
+/////start
+clear()
+
+     let begin = await showTemplateScreen("rickroll");
+        pause(10);
+        await waitForKey();
+        rickrolled.pause();
+        begin.remove();
+        	clear()
+				resolve();
+	clear()
+
+
+
+
+	});
+    
+
+}
+
+
+
+const stylesheets = ["rick"];
+const templates = ["rick"];
+export default rick;
+export { templates,stylesheets };
